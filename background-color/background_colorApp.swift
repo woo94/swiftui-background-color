1f03d9bf424f1521c27f52f00024e116f9eb48df
woo94 <bestman21c@gmail.com>
2023-01-21 14:10:56 +0900

    Initial Commit

---
+++ background-color/background_colorApp.swift
@@ -0,0 +1,17 @@
+//
+//  background_colorApp.swift
+//  background-color
+//
+//  Created by woo94 on 2023/01/21.
+//
+
+import SwiftUI
+
+@main
+struct background_colorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
